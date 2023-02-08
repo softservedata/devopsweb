dc348d273ed52cc2d977857f85b4c809257daed4
Yaroslav <garasym@gmail.com>
2023-02-09 00:09:22 +0200

    update index.json

--- JenkinsPipeline/intro/foreground.sh
+++ JenkinsPipeline/intro/foreground.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo off
+stty -echo
 
-while [ ! -f /tmp/backgroundintro ]
- do
-  sleep 2
-  echo -n "."
-done
+while [ ! -f /tmp/backgroundintro ]; do sleep 2; echo -n "."; done
 
 echo "Jenkins is up and running"
