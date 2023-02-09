bd71f2105ec7c3c4ae36eccef05195534a48894e
Yaroslav <garasym@gmail.com>
2023-02-09 13:29:03 +0200

    update script

--- JenkinsPipeline/finish/background.sh
+++ JenkinsPipeline/finish/background.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sleep 3
 echo "done"
 echo "done" > /tmp/background8
--- JenkinsPipeline/finish/foreground.sh
+++ JenkinsPipeline/finish/foreground.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+stty -echo
 echo "Waiting for finish-background-script to finish"
 while [ ! -f /tmp/background8 ]; do sleep 1; done
 echo "done"
+stty echo
