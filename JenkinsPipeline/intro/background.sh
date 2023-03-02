0ee26ea479ef2556a245aec81a4c1054d7a74db6
Yaroslav <garasym@gmail.com>
2023-03-02 10:36:14 +0200

    background updated

--- JenkinsPipeline/intro/background.sh
+++ JenkinsPipeline/intro/background.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd /root
-docker volume create jenkins_home
-git clone https://github.com/softservedata/devopsjen.git
-cp -R devopsjen/.jenkins/* /var/lib/docker/volumes/jenkins_home/_data
+
+docker pull ghcr.io/softservedata/devopsjen:main
 sleep 2
-chown -R ubuntu:ubuntu /var/lib/docker/volumes/jenkins_home
-cd devopsjen
-docker build -t alpinejenkins .
-docker run --rm --detach --name jenkins_server -p 8080:8080 -v jenkins_home:/root/.jenkins alpinejenkins
+docker run --rm --detach --name jenkins_server -p 8080:8080 -v jenkins_home:/root/.jenkins ghcr.io/softservedata/devopsjen:main
+
+#docker volume create jenkins_home
+#git clone https://github.com/softservedata/devopsjen.git
+#cp -R devopsjen/.jenkins/* /var/lib/docker/volumes/jenkins_home/_data
+#sleep 2
+#chown -R ubuntu:ubuntu /var/lib/docker/volumes/jenkins_home
+#cd devopsjen
+#docker build -t alpinejenkins .
+#docker run --rm --detach --name jenkins_server -p 8080:8080 -v jenkins_home:/root/.jenkins alpinejenkins
